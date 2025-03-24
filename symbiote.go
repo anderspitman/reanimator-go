6f55749e4e8c69bbe0e72e47d85ca7d592030dc9
Anders Pitman <&ers@apitman.com>
2025-03-24 09:56:04 -0600

    Invert logic
    Now only runs supervised if the proper flag is there, instead of
    if it's missing

--- symbiote.go
+++ symbiote.go
@@ -8,15 +8,23 @@ import (
 )
 
 func Supervise() {
-	if len(os.Args) >= 2 && os.Args[1] == "-symbiote-supervised" {
-		os.Args = append(os.Args[:1], os.Args[2:]...)
-		return
-	}
+
+        foundIdx := -1
+        for i, arg := range os.Args {
+                if arg == "-symbiote-supervised" {
+                        foundIdx = i
+                }
+        }
+
+        if foundIdx < 0 {
+                return
+        }
+
+        args := append(os.Args[:foundIdx], os.Args[foundIdx+1:]...)
 
 	for {
 		fmt.Fprintf(os.Stderr, "symbiote supervisor - Starting process\n")
-		args := append([]string{"-symbiote-supervised"}, os.Args[1:]...)
-		cmd := exec.Command(os.Args[0], args...)
+                cmd := exec.Command(args[0], args[1:]...)
 		cmd.Stdin = os.Stdin
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
